a959929fe32e3230cf054e7e5c0fabd4fd3ae6ff
Brandon Kaid <brandonkaid@gmail.com>
2016-11-20 16:12:54 -0700

    Project Euler 32

--- project-euler/problem-0032/index.js
+++ project-euler/problem-0032/index.js
@@ -1,7 +1,7 @@
 'use strict';
 const problem32 = require('./problem-0032');
 
-let n = 7;
+let n = 9;
 
 module.exports = {
   description: `Find the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through ${n} pandigital`,
